9e1534db69e15532fb8ac717ab2250ed9a00dd75
Daniel Rey Lopez <daniel.rey.lopez@automattic.com>
2017-05-10 15:30:45 +0100

    Added logic for submitting changes to the API

--- client/extensions/woocommerce/state/settings/shipping/zones/actions.js
+++ client/extensions/woocommerce/state/settings/shipping/zones/actions.js
@@ -9,6 +9,7 @@ import { omit, sortBy, first, remove } from 'lodash';
 import {
 	WOOCOMMERCE_SHIPPING_METHODS_FETCH_ERROR,
 	WOOCOMMERCE_SHIPPING_METHODS_FETCH_SUCCESS,
+	WOOCOMMERCE_SHIPPING_SETTINGS_SAVE_SUCCESS,
 	WOOCOMMERCE_SHIPPING_ZONE_ADD,
 	WOOCOMMERCE_SHIPPING_ZONE_CANCEL,
 	WOOCOMMERCE_SHIPPING_ZONE_CLOSE,
@@ -28,6 +29,7 @@ import {
 	WOOCOMMERCE_SHIPPING_ZONES_FETCH_SUCCESS,
 } from '../../../action-types';
 import * as api from '../../../helpers/api';
+import { getOperationsToSaveZonesSettings } from './selectors';
 
 const removeOrder = ( collection ) => collection.map( ( elem ) => omit( elem, 'order' ) );
 const removeLinks = ( data ) => omit( data, '_links' );
@@ -73,8 +75,49 @@ export const fetchServerData = () => ( dispatch, getState ) => {
 		.catch( ( error ) => dispatch( { type: WOOCOMMERCE_SHIPPING_METHODS_FETCH_ERROR, error } ) );
 };
 
-export const submitChanges = () => ( /* dispatch, getState */ ) => {
-	// TODO: Not implemented yet
+const getApiRequest = ( action, payload, id, zoneId, state ) => {
+	switch ( action ) {
+		case 'add_method':
+			return api.post( `shipping/zones/${ zoneId }/methods`, payload, state );
+		case 'update_method':
+			return api.put( `shipping/zones/${ zoneId }/methods/${ id }`, payload, state );
+		case 'delete_method':
+			return api.del( `shipping/zones/${ zoneId }/methods/${ id }`, state );
+		case 'update_locations':
+			return api.put( `shipping/zones/${ zoneId }/locations`, payload, state );
+		case 'add_zone':
+			return api.post( 'shipping/zones', payload, state );
+		case 'update_zone':
+			return api.put( `shipping/zones/${ zoneId }`, payload, state );
+		case 'delete_zone':
+			return api.del( `shipping/zones/${ zoneId }`, state );
+		default:
+			throw new Error( 'Unrecognized action "' + action + '"' );
+	}
+};
+
+const runOperations = ( operations, state ) => {
+	return Promise.all( operations.map( ( { action, payload, id, zoneId, subOperations } ) => {
+		const request = getApiRequest( action, payload, id, zoneId, state );
+		return request.then( ( result ) => {
+			if ( ! operations ) {
+				return;
+			}
+			return Promise.all( subOperations.map( ( subOperationFunc ) => {
+				return runOperations( subOperationFunc( result.id || result.instance_id ), state );
+			} ) );
+		} );
+	} ) );
+};
+
+export const submitChanges = () => ( dispatch, getState ) => {
+	// TODO: Put individual errors in the state so they can be displayed in the UI
+	// TODO: Update the serverZones state tree with each success response so it always reflect the server state
+	const state = getState();
+	runOperations( getOperationsToSaveZonesSettings( state ), state )
+	// TODO: Show some kind of success notice
+	// TODO: This only saves the zone settings, not the entire page
+		.then( () => dispatch( { type: WOOCOMMERCE_SHIPPING_SETTINGS_SAVE_SUCCESS } ) );
 };
 
 export const addShippingZone = () => ( { type: WOOCOMMERCE_SHIPPING_ZONE_ADD } );
---
+++ client/extensions/woocommerce/state/settings/shipping/zones/selectors.js
@@ -0,0 +1,120 @@
+/**
+ * External dependencies
+ */
+import { get, remove, keyBy, omit, flatten, isEqual, xorBy, pick } from 'lodash';
+
+const getOperationsToUpdateMethod = ( method, serverMethod, zoneId ) => ( instanceId ) => {
+	if ( ! method ) {
+		return [ {
+			action: 'delete_method',
+			zoneId,
+			id: instanceId,
+		} ];
+	}
+
+	if ( ! serverMethod || ! isEqual( method, serverMethod ) ) {
+		return [ {
+			action: 'update_method',
+			zoneId,
+			id: instanceId,
+			payload: {
+				...pick( method, 'enabled' ),
+				settings: omit( method, [ 'id', 'enabled', 'method_id' ] ),
+			},
+		} ];
+	}
+};
+
+const getOperationsToAddMethod = ( method, zoneId ) => {
+	return [ {
+		action: 'add_method',
+		zoneId,
+		payload: pick( method, 'method_id' ),
+		subOperations: [
+			getOperationsToUpdateMethod( method, null, zoneId ),
+		],
+	} ];
+};
+
+const getOperationsToUpdateMethods = ( methods, serverMethods ) => ( zoneId ) => {
+	methods = [ ...methods ];
+	const newMethods = remove( methods, { id: null } );
+	methods = keyBy( methods, 'id' );
+
+	return [
+		...flatten( newMethods.map( ( method ) => getOperationsToAddMethod( method, zoneId ) ) ),
+		...flatten( serverMethods.map( ( serverMethod ) =>
+			getOperationsToUpdateMethod( methods[ serverMethod.id ], serverMethod, zoneId )( serverMethod.id ) )
+		),
+	];
+};
+
+const getOperationsToUpdateLocations = ( locations, serverLocations ) => ( zoneId ) => {
+	if ( ! xorBy( locations, serverLocations, isEqual ).length ) {
+		return [];
+	}
+	return [ {
+		action: 'update_locations',
+		payload: locations,
+		zoneId,
+	} ];
+};
+
+const getOperationsToUpdateZone = ( zone, serverZone ) => {
+	if ( ! zone ) {
+		return [ {
+			action: 'delete_zone',
+			zoneId: serverZone.id,
+		} ];
+	}
+
+	if ( ! serverZone ) {
+		return [ {
+			action: 'add_zone',
+			payload: omit( zone, [ 'id', 'locations', 'methods' ] ),
+			subOperations: [
+				getOperationsToUpdateMethods( zone.methods, null ),
+				getOperationsToUpdateLocations( zone.locations, null ),
+			],
+		} ];
+	}
+
+	const operations = [];
+	const zoneInfo = omit( zone, [ 'locations', 'methods' ] );
+	const serverZoneInfo = omit( serverZone, [ 'locations', 'methods' ] );
+	if ( ! isEqual( zoneInfo, serverZoneInfo ) ) {
+		operations.push( {
+			action: 'update_zone',
+			zoneId: zone.id,
+			payload: zoneInfo,
+		} );
+	}
+
+	return [ ...operations,
+		...getOperationsToUpdateMethods( zone.methods, serverZone.methods )( zone.id ),
+		...getOperationsToUpdateLocations( zone.locations, serverZone.locations )( zone.id ),
+	];
+};
+
+export const getOperationsToSaveZonesSettings = ( state ) => {
+	const data = get( state, [ 'extensions', 'woocommerce', 'settings', 'shipping', 'zones' ] );
+	let zones = [ ...data.zones ];
+	const serverZones = [ ...data.serverZones ];
+	const restOfWorldServerZone = remove( serverZones, { id: 0 } )[ 0 ];
+	const restOfWorldZone = remove( zones, { id: 0 } )[ 0 ];
+	if ( ! restOfWorldServerZone ) {
+		throw new Error( 'The server didn\'t provide a "Rest Of The World" shipping zone' );
+	}
+	if ( ! restOfWorldZone ) {
+		throw new Error( 'The "Rest Of The World" shipping zone has been deleted' );
+	}
+	const newZones = remove( zones, { id: null } );
+	zones = keyBy( zones, 'id' );
+
+	return [
+		...flatten( newZones.map( ( zone ) => getOperationsToUpdateZone( zone, null ) ) ),
+		...flatten( serverZones.map( ( serverZone ) => getOperationsToUpdateZone( zones[ serverZone.id ], serverZone ) ) ),
+		// "Rest of the world" zone can't have custom name or locations, so only look at their differences in methods
+		...getOperationsToUpdateMethods( restOfWorldZone.methods, restOfWorldServerZone.methods )( 0 ),
+	];
+};
